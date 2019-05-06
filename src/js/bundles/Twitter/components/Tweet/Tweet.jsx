8d84a228824a1a214fd732ab5a2df7d7177508c0
Justin Schellenberg <jrschellenberg@gmail.com>
2019-05-05 20:36:42 -0500

    Fix Twitter URL Links

--- src/js/bundles/Twitter/components/Tweet/Tweet.jsx
+++ src/js/bundles/Twitter/components/Tweet/Tweet.jsx
@@ -12,9 +12,18 @@ export const Tweet = ({loading, user, retweeted_status, id_str, text, truncated,
     )
   }
   
+  let url;
+  if(entities && entities.urls && entities.urls[0] && entities.urls[0].url) {
+    url = entities.urls[0].url;
+  }
+  else if(retweeted_status && retweeted_status.entities && retweeted_status.entities.urls && retweeted_status.entities.urls[0] && retweeted_status.entities.urls[0].url ){
+    url = retweeted_status.entities.urls[0].url;
+  }
+  else {
+    url = 'https://twitter.com/CFMSFEMC';
+  }
+  
   const tweetText = retweeted_status ? retweeted_status.text : text;
-  const url = entities && entities.urls && entities.urls[0] && entities.urls[0].url ? entities.urls[0].url : 'https://twitter.com/CFMSFEMC' ;
-
   
   return (
     <div className={`Tweet`}>
